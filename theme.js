fe4106612ef6d4b453bd8cf2113c58bb94194490
David Luecke <daff@neyeon.com>
2019-03-26 08:40:06 -0700

    Start setup for VanJS talk

---
+++ theme.js
@@ -0,0 +1,28 @@
+export default {
+  googleFont: 'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Raleway:100,300,400,500,700',
+  // add a custom font
+  font: 'Roboto,Helvetica Neue,Helvetica,Arial,sans-serif',
+  css: {
+    fontWeight: 200
+  },
+  // custom colors
+  colors: {
+  },
+  a: {
+    color: '#e4517f'
+  },
+  h2: {
+    fontWeight: 200,
+    color: '#e4517f'
+  },
+  h1: {
+    color: '#4b4df1',
+    fontWeight: 300
+  },
+  li: {
+    fontSize: '42px'
+  },
+  p: {
+    fontSize: '42px'
+  }
+}
